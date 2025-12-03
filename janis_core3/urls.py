2cdf2cf8cffd6f1ab9f301409f2401a025152a55
propifygit <sistemapropify@gmail.com>
2025-12-03 15:33:41 -0500

    feat(api/security): add refresh token revoke endpoint (blacklist)

--- janis_core3/urls.py
+++ janis_core3/urls.py
@@ -20,6 +20,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .api_views import revoke_refresh_token
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -33,6 +34,7 @@ urlpatterns = [
     # JWT token endpoints for mobile clients
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/revoke/', revoke_refresh_token, name='token_revoke'),
 ]
 
 # Servir archivos de media en desarrollo
